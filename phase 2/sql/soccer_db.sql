4d29812e99f3bca15530dc721d6090e21ee53b46
M17SN <mihsn7778@gmail.com>
2025-04-24 23:24:22 +0300

    added soccer_ddl schema

---
+++ phase 2/sql/soccer_db.sql
@@ -0,0 +1,284 @@
+-- CREATE DATABASE soccerdb;
+USE soccerdb;
+CREATE TABLE TOURNAMENT (
+tr_id numeric NOT NULL,
+tr_name character varying(40) NOT NULL,
+start_date date NOT NULL,
+end_date date NOT NULL,
+PRIMARY KEY (tr_id) );
+INSERT INTO TOURNAMENT VALUES (1, 'Faculty Tournament', '2023-03-10', '2023-03-25');
+INSERT INTO TOURNAMENT VALUES (2, 'Open Tournament', '2023-03-15', '2023-03-30');
+INSERT INTO TOURNAMENT VALUES (3, 'Student Tournament', '2022-12-10', '2022-12-02');
+INSERT INTO TOURNAMENT VALUES (4, 'Staff Tournament', '2023-02-15', '2023-02-25');
+INSERT INTO TOURNAMENT VALUES (5, 'Annual Tournament', '2023-01-01', '2023-01-15');
+CREATE TABLE VENUE (
+venue_id numeric NOT NULL,
+venue_name character varying(30) NOT NULL,
+venue_status character(1) NOT NULL,
+venue_capacity numeric NOT NULL,
+PRIMARY KEY (venue_id) );
+INSERT INTO VENUE VALUES (11, 'Main Stadium', 'Y', 20000);
+INSERT INTO VENUE VALUES (22, 'Indoor Stadium', 'Y', 1000);
+INSERT INTO VENUE VALUES (33, 'Jabal Field', 'N', 500);
+INSERT INTO VENUE VALUES (44, 'Student Field', 'Y', 2000);
+CREATE TABLE TEAM (
+team_id numeric NOT NULL,
+team_name character varying(30) NOT NULL,
+PRIMARY KEY (team_id));
+INSERT INTO TEAM VALUES (1214,'CCM');
+INSERT INTO TEAM VALUES (1215,'KBS');
+INSERT INTO TEAM VALUES (1216,'CEP');
+INSERT INTO TEAM VALUES (1217,'CPG');
+INSERT INTO TEAM VALUES (1218,'CCM');
+INSERT INTO TEAM VALUES (1219,'CDB');
+INSERT INTO TEAM VALUES (1220,'CGS');
+CREATE TABLE TOURNAMENT_TEAM (
+team_id numeric NOT NULL,
+tr_id numeric NOT NULL,
+team_group character(1) NOT NULL,
+match_played numeric NOT NULL,
+won numeric NOT NULL,
+draw numeric NOT NULL,
+lost numeric NOT NULL,
+goal_for numeric NOT NULL,
+goal_against numeric NOT NULL,
+goal_diff numeric NOT NULL,
+points numeric NOT NULL,
+group_position numeric NOT NULL,
+PRIMARY KEY (team_id, tr_id),
+FOREIGN KEY (tr_id) REFERENCES TOURNAMENT (tr_id),
+FOREIGN KEY (team_id) REFERENCES TEAM (team_id));
+INSERT INTO TOURNAMENT_TEAM VALUES (1214,1,'A',3,0,3,0,4,4,0,3,1);
+INSERT INTO TOURNAMENT_TEAM VALUES (1215,1,'B',3,1,1,1,3,4,-1,4,2);
+INSERT INTO TOURNAMENT_TEAM VALUES (1216,2,'C',3,1,1,1,0,0,0,4,2);
+INSERT INTO TOURNAMENT_TEAM VALUES (1217,2,'A',3,1,1,1,1,4,-3,4,1);
+INSERT INTO TOURNAMENT_TEAM VALUES (1218,3,'A',3,1,1,1,2,4,-2,4,3);
+INSERT INTO TOURNAMENT_TEAM VALUES (1219,3,'B',3,1,1,1,4,2,2,4,1);
+INSERT INTO TOURNAMENT_TEAM VALUES (1220,3,'C',3,1,1,1,1,2,-1,4,2);
+INSERT INTO TOURNAMENT_TEAM VALUES (1214,3,'C',3,1,1,1,1,2,-1,4,2);
+CREATE TABLE PERSON (
+kfupm_id numeric NOT NULL,
+name character varying(40) NOT NULL,
+date_of_birth date,
+PRIMARY KEY (kfupm_id));
+INSERT INTO PERSON VALUES (1001, 'Ahmed', '1999-03-10');
+INSERT INTO PERSON VALUES (1003, 'Saeed', '2005-03-10');
+INSERT INTO PERSON VALUES (1005, 'Majid', '1996-03-10');
+INSERT INTO PERSON VALUES (1007, 'Ahmed', '2001-03-10');
+INSERT INTO PERSON VALUES (1009, 'Fahd', '2008-03-10');
+INSERT INTO PERSON VALUES (1011, 'Mohammed', '1998-03-10');
+INSERT INTO PERSON VALUES (1013, 'Raed', '1999-07-10');
+INSERT INTO PERSON VALUES (1015, 'Mousa', '2009-03-10');
+INSERT INTO PERSON VALUES (1017, 'Ali', '2009-03-10');
+INSERT INTO PERSON VALUES (1019, 'Yasir', '2007-03-10');
+INSERT INTO PERSON VALUES (1021, 'Ashraf', '2010-03-10');
+INSERT INTO PERSON VALUES (1023, 'Hassan', '2004-03-10');
+INSERT INTO PERSON VALUES (1025, 'Abdullah', '2003-03-10');
+INSERT INTO PERSON VALUES (1027, 'Bassam', '2005-03-10');
+INSERT INTO PERSON VALUES (1029, 'Ashraf', '2004-03-10');
+INSERT INTO PERSON VALUES (9001,'Carlos', '2004-03-10');
+INSERT INTO PERSON VALUES (9003,'Farhan', '2004-03-10');
+INSERT INTO PERSON VALUES (9002,'Jameel', '2004-03-10');
+INSERT INTO PERSON VALUES (7001,'Hassan', '2004-03-10');
+INSERT INTO PERSON VALUES (7002,'Robert', '2004-03-10');
+INSERT INTO PERSON VALUES (7003,'Fayez', '2004-03-10');
+INSERT INTO PERSON VALUES (7004, 'Mark', '2004-03-10');
+INSERT INTO PERSON VALUES (7005,'Ahmad', '2004-03-10');
+INSERT INTO PERSON VALUES (7006,'Faisal', '2004-03-10');
+INSERT INTO PERSON VALUES (7007,'Noman', '2004-03-10');
+INSERT INTO PERSON VALUES (3001,'Ahmed', '2004-03-10');
+INSERT INTO PERSON VALUES (3003,'Saied', '2004-03-10');
+INSERT INTO PERSON VALUES (3002,'Fadhel', '2004-03-10');
+INSERT INTO PERSON VALUES (3004,'Morad', '2004-03-10');
+INSERT INTO PERSON VALUES (3005,'Farid', '2004-03-10');
+CREATE TABLE PLAYING_POSITION (
+position_id character(2) NOT NULL,
+position_desc character varying(15) NOT NULL,
+PRIMARY KEY (position_id) );
+INSERT INTO PLAYING_POSITION VALUES ('GK', 'Goalkeepers');
+INSERT INTO PLAYING_POSITION VALUES ('DF', 'Defenders');
+INSERT INTO PLAYING_POSITION VALUES ('MF', 'Midfielders');
+INSERT INTO PLAYING_POSITION VALUES ('FD', 'Forwards');
+CREATE TABLE PLAYER (
+player_id numeric NOT NULL,
+jersey_no numeric NOT NULL,
+position_to_play character(2) NOT NULL,
+PRIMARY KEY (player_id),
+FOREIGN KEY (player_id) REFERENCES PERSON (kfupm_id),
+FOREIGN KEY (position_to_play) REFERENCES PLAYING_POSITION (position_id));
+INSERT INTO PLAYER VALUES (1001,1, 'GK');
+INSERT INTO PLAYER VALUES (1003,2, 'DF');
+INSERT INTO PLAYER VALUES (1005,3, 'DF');
+INSERT INTO PLAYER VALUES (1007,4, 'MF');
+INSERT INTO PLAYER VALUES (1009,5, 'FD');
+INSERT INTO PLAYER VALUES (1011,6, 'FD');
+CREATE TABLE TEAM_PLAYER (
+player_id numeric NOT NULL,
+team_id numeric NOT NULL,
+tr_id numeric NOT NULL,
+PRIMARY KEY (player_id, team_id, tr_id),
+FOREIGN KEY (player_id) REFERENCES PLAYER (player_id),
+FOREIGN KEY (team_id) REFERENCES TEAM (team_id),
+FOREIGN KEY (tr_id) REFERENCES TOURNAMENT (tr_id));
+INSERT INTO TEAM_PLAYER VALUES (1001,1214,1);
+INSERT INTO TEAM_PLAYER VALUES (1003,1216,2);
+INSERT INTO TEAM_PLAYER VALUES (1001,1215,2);
+INSERT INTO TEAM_PLAYER VALUES (1003,1216,1);
+INSERT INTO TEAM_PLAYER VALUES (1005,1216,1);
+INSERT INTO TEAM_PLAYER VALUES (1005,1218,2);
+INSERT INTO TEAM_PLAYER VALUES (1007,1217,1);
+INSERT INTO TEAM_PLAYER VALUES (1009,1218,1);
+INSERT INTO TEAM_PLAYER VALUES (1011,1219,3);
+CREATE TABLE SUPPORT (
+support_type character(2) NOT NULL,
+support_desc character varying(15) NOT NULL,
+PRIMARY KEY (support_type));
+INSERT INTO SUPPORT VALUES ('RF', 'REFEREE');
+INSERT INTO SUPPORT VALUES ('AR', 'ASST REFEREE');
+INSERT INTO SUPPORT VALUES ('CH', 'COACH');
+INSERT INTO SUPPORT VALUES ('AC', 'ASST COACH');
+CREATE TABLE TEAM_SUPPORT (
+support_id numeric NOT NULL,
+team_id numeric NOT NULL,
+tr_id numeric NOT NULL,
+support_type character(2) NOT NULL,
+PRIMARY KEY (support_id, team_id, tr_id),
+FOREIGN KEY (support_id) REFERENCES PERSON (kfupm_id),
+FOREIGN KEY (team_id) REFERENCES TEAM (team_id),
+FOREIGN KEY (tr_id) REFERENCES TOURNAMENT (tr_id),
+FOREIGN KEY (support_type) REFERENCES SUPPORT (support_type));
+INSERT INTO TEAM_SUPPORT VALUES (9001,1214,1,'CH');
+INSERT INTO TEAM_SUPPORT VALUES (9002,1216,1,'CH');
+INSERT INTO TEAM_SUPPORT VALUES (7001,1214,1,'AC');
+INSERT INTO TEAM_SUPPORT VALUES (7002,1216,1,'AC');
+CREATE TABLE MATCH_PLAYED (
+match_no numeric NOT NULL,
+play_stage character(1) NOT NULL,
+play_date date NOT NULL,
+team_id1 numeric NOT NULL,
+team_id2 numeric NOT NULL,
+results character(5) NOT NULL,
+decided_by character(1) NOT NULL,
+goal_score character(5) NOT NULL,
+venue_id numeric NOT NULL,
+audience numeric NOT NULL,
+player_of_match numeric NOT NULL,
+stop1_sec numeric NOT NULL,
+stop2_sec numeric NOT NULL,
+PRIMARY KEY (match_no),
+FOREIGN KEY (team_id1) REFERENCES TEAM (team_id),
+FOREIGN KEY (team_id2) REFERENCES TEAM (team_id),
+FOREIGN KEY (venue_id) REFERENCES VENUE (venue_id),
+FOREIGN KEY (player_of_match) REFERENCES PLAYER (player_id));
+INSERT INTO MATCH_PLAYED VALUES (1, 'G', '2020-03-11', 1214, 1215, 'WIN', 'N', '2-1',11,5113,1007,131,242);
+INSERT INTO MATCH_PLAYED VALUES (2, 'G', '2020-03-11', 1215,1216,'DRAW', 'N', '1-1',22,510,1009,111,272);
+INSERT INTO MATCH_PLAYED VALUES (3, 'F', '2020-03-12', 1214, 1215,'LOSS', 'N', '1-3',33,2510,1003,111,172);
+INSERT INTO MATCH_PLAYED VALUES (4, 'G', '2020-03-13', 1214, 1215,'WIN', 'N', '5-1',11,1510,1011,111,372);
+CREATE TABLE match_details (
+match_no numeric NOT NULL,
+team_id numeric NOT NULL,
+win_lose character(1) NOT NULL,
+decided_by character(1) NOT NULL,
+goal_score numeric NOT NULL,
+penalty_score numeric,
+player_gk numeric NOT NULL,
+PRIMARY KEY (match_no, team_id),
+FOREIGN KEY (team_id) REFERENCES TEAM (team_id),
+FOREIGN KEY (player_gk) REFERENCES PLAYER (player_id),
+FOREIGN KEY (match_no) REFERENCES MATCH_PLAYED (match_no));
+INSERT INTO match_details VALUES (1, 1214, 'W', 'N', 1, 0,1001);
+INSERT INTO match_details VALUES (2, 1215, 'W', 'N', 2, 0,1003);
+INSERT INTO match_details VALUES (2, 1217, 'L', 'N', 2, 0,1003);
+INSERT INTO match_details VALUES (1, 1216, 'L', 'N', 1, 0,1001);
+INSERT INTO match_details VALUES (3, 1215, 'W', 'N', 2, 0,1003);
+INSERT INTO match_details VALUES (3, 1214, 'W', 'N', 2, 0,1005);
+CREATE TABLE MATCH_SUPPORT (
+match_no numeric NOT NULL,
+support_id numeric NOT NULL,
+support_type character(2) NOT NULL,
+PRIMARY KEY (match_no, support_id),
+FOREIGN KEY (match_no) REFERENCES MATCH_PLAYED (match_no),
+FOREIGN KEY (support_id) REFERENCES PERSON (kfupm_id));
+INSERT INTO MATCH_SUPPORT VALUES (1,3002,'RF');
+INSERT INTO MATCH_SUPPORT VALUES (1,3003,'AR');
+INSERT INTO MATCH_SUPPORT VALUES (2,3001,'RF');
+INSERT INTO MATCH_SUPPORT VALUES (2,3003,'AR');
+CREATE TABLE GOAL_DETAILS (
+goal_id numeric NOT NULL,
+match_no numeric NOT NULL,
+player_id numeric NOT NULL,
+team_id numeric NOT NULL,
+goal_time numeric NOT NULL,
+goal_type character(1) NOT NULL,
+play_stage character(1) NOT NULL,
+goal_schedule character(2) NOT NULL,
+goal_half numeric,
+PRIMARY KEY (goal_id),
+FOREIGN KEY (team_id) REFERENCES TEAM (team_id),
+FOREIGN KEY (player_id) REFERENCES PLAYER (player_id),
+FOREIGN KEY (match_no) REFERENCES MATCH_PLAYED (match_no));
+INSERT INTO GOAL_DETAILS VALUES (1, 1, 1003, 1214, 72, 'N', 'G', 'NT',2);
+INSERT INTO GOAL_DETAILS VALUES (2, 1, 1005, 1214, 82, 'N', 'G', 'NT',2);
+INSERT INTO GOAL_DETAILS VALUES (3, 1, 1003, 1214, 77, 'N', 'G', 'NT',2);
+INSERT INTO GOAL_DETAILS VALUES (4, 1, 1005, 1214, 92, 'N', 'G', 'NT',1);
+CREATE TABLE PENALTY_SHOOTOUT (
+kick_id numeric NOT NULL,
+match_no numeric NOT NULL,
+team_id numeric NOT NULL,
+player_id numeric NOT NULL,
+score_goal character(1) NOT NULL,
+kick_no numeric NOT NULL,
+PRIMARY KEY (kick_id),
+FOREIGN KEY (team_id) REFERENCES TEAM (team_id),
+FOREIGN KEY (player_id) REFERENCES PLAYER (player_id),
+FOREIGN KEY (match_no) REFERENCES MATCH_PLAYED (match_no));
+INSERT INTO PENALTY_SHOOTOUT VALUES (1, 1, 1215, 1003, 'N', 1);
+INSERT INTO PENALTY_SHOOTOUT VALUES (2, 2, 1217, 1007, 'Y', 2);
+CREATE TABLE PLAYER_BOOKED (
+match_no numeric NOT NULL,
+team_id numeric NOT NULL,
+player_id numeric NOT NULL,
+booking_time numeric NOT NULL,
+sent_off character(1),
+play_schedule character(2) NOT NULL,
+play_half numeric NOT NULL,
+PRIMARY KEY (match_no, team_id, player_id),
+FOREIGN KEY (team_id) REFERENCES TEAM (team_id),
+FOREIGN KEY (player_id) REFERENCES PLAYER (player_id),
+FOREIGN KEY (match_no) REFERENCES MATCH_PLAYED (match_no));
+INSERT INTO PLAYER_BOOKED VALUES (1, 1215, 1003, 36, 'N','NT', 1);
+INSERT INTO PLAYER_BOOKED VALUES (1, 1217, 1005, 76, 'Y','NT', 2);
+CREATE TABLE PLAYER_IN_OUT (
+match_no numeric NOT NULL,
+team_id numeric NOT NULL,
+player_id numeric NOT NULL,
+in_out character(1) NOT NULL,
+time_in_out numeric NOT NULL,
+play_schedule character(2) NOT NULL,
+play_half numeric NOT NULL,
+PRIMARY KEY (match_no, team_id),
+FOREIGN KEY (team_id) REFERENCES TEAM (team_id),
+FOREIGN KEY (player_id) REFERENCES PLAYER (player_id),
+FOREIGN KEY (match_no) REFERENCES MATCH_PLAYED (match_no));
+INSERT INTO PLAYER_IN_OUT VALUES (1, 1214, 1003, 'I',73,'NT', 2);
+INSERT INTO PLAYER_IN_OUT VALUES (2, 1215, 1007, 'O',33,'NT', 1);
+CREATE TABLE MATCH_CAPTAIN (
+match_no numeric NOT NULL,
+team_id numeric NOT NULL,
+player_captain numeric NOT NULL,
+PRIMARY KEY (match_no, team_id),
+FOREIGN KEY (team_id) REFERENCES TEAM (team_id),
+FOREIGN KEY (player_captain) REFERENCES PLAYER (player_id),
+FOREIGN KEY (match_no) REFERENCES MATCH_PLAYED (match_no));
+INSERT INTO MATCH_CAPTAIN VALUES (1, 1214, 1005);
+INSERT INTO MATCH_CAPTAIN VALUES (2, 1215, 1003);
+CREATE TABLE PENALTY_GK (
+match_no numeric NOT NULL,
+team_id numeric NOT NULL,
+player_gk numeric NOT NULL,
+PRIMARY KEY (match_no, team_id),
+FOREIGN KEY (team_id) REFERENCES TEAM (team_id),
+FOREIGN KEY (player_gk) REFERENCES PLAYER (player_id),
+FOREIGN KEY (match_no) REFERENCES MATCH_PLAYED (match_no));
+INSERT INTO PENALTY_GK VALUES (1, 1214, 1003);
+INSERT INTO PENALTY_GK VALUES (1, 1215, 1007);
